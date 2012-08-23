84a6fb5224ef163ceb260c8710caef145eaa0d04
Tim Koopmans <tim.koops@gmail.com>
2012-08-23 20:56:40 +1000

    airbrake only in prod

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,4 +1,4 @@
-unless Rails.env.development?
+if Rails.env.production?
   Airbrake.configure do |config|
     config.api_key = '29890054-ccec-4a4d-aec1-2b8966d4bb0f'
     config.host = 'crashlog.io'
