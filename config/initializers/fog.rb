ebba833481d01b80c37da71a181ea6e976a41242
Tim Koopmans <tim.koops@gmail.com>
2012-09-03 15:38:23 +1000

    Localising fonts

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -6,3 +6,6 @@ $fog = Fog::Compute.new(
   :aws_access_key_id      => config['aws_access_key_id'],
   :aws_secret_access_key  => config['aws_secret_access_key']
 )
+$slave_image_ids = {
+  :ap_southeast_1 => config['ap_southeast_1']
+}
