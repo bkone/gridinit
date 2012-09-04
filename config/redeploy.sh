aeaeb978e2863dd45e24940effa27cf231cc4ae4
Tim Koopmans <tim.koops@gmail.com>
2012-09-05 09:08:43 +1000

    Added redeploy script for existing nodes

--- config/redeploy.sh
+++ config/redeploy.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
+# must run this as root
+# curl -L https://raw.github.com/altentee/gridinit/master/config/redeploy.sh | bash
 exec > >(tee /var/log/reinstall.log)
 exec 2>&1
 rm -rf /var/gridnode
+rm -rf /tmp/altentee*
 mkdir /var/gridnode
 chown -R ubuntu /var/gridnode
-touch /var/log/gridnode-jmeter.log
-touch /var/log/gridnode-watirwebdriver.log
-touch /var/log/gridnode-stdout.log
-touch /var/log/gridnode-stderr.log
-touch /var/log/gridnode-errors.log
-chown -R ubuntu /var/log/gridnode-*
 cd /tmp
-  wget -O gridinit.zip https://github.com/altentee/gridinit/zipball/master
-  unzip gridinit.zip
-  cd  altentee-gridinit*
-  gem install bundler --no-ri --no-rdoc
-  gem install capistrano --no-ri --no-rdoc
-  ssh-keygen -t dsa -f /root/.ssh/id_dsa -N ''
-  cat /root/.ssh/id_dsa.pub >> /root/.ssh/authorized_keys
-  chmod 600 /root/.ssh/authorized_keys
-  cat - << EOS > ~/.ssh/config
+wget -O gridinit.zip https://github.com/altentee/gridinit/zipball/master
+unzip gridinit.zip
+cd  altentee-gridinit*
+gem install bundler --no-ri --no-rdoc
+gem install capistrano --no-ri --no-rdoc
+rm -f /root/.ssh/id*
+ssh-keygen -t dsa -f /root/.ssh/id_dsa -N ''
+cat /root/.ssh/id_dsa.pub >> /root/.ssh/authorized_keys
+chmod 600 /root/.ssh/authorized_keys
+cat - << EOS > ~/.ssh/config
 Host *
 CheckHostIP no
 StrictHostKeyChecking no
