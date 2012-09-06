2ab7b3a0d2f429bc0d05c7b69f2f34ecc136a769
Tim Koopmans <tim.koops@gmail.com>
2012-09-06 10:16:42 +1000

    emg prod bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,8 +5,8 @@ Gridinit::Application.configure do
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
 
   config.action_mailer.default_url_options = { :host => 'gridinit.com' }
   
